936883924e5eea362a9661c893992eaeee29f513
jengryg <62914315+jengryg@users.noreply.github.com>
2025-03-25 17:04:37 +0100

    Change to gradle version catalog, add and configure gradle build tasks.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,66 +1,140 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.4.4"
-	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("plugin.jpa") version "1.9.25"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency)
+    alias(libs.plugins.kotlin.jpa)
+
+    jacoco
+    idea
 }
 
 group = "game"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.micrometer:micrometer-tracing-bridge-brave")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.liquibase:liquibase-core")
-	implementation("org.springframework.session:spring-session-core")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("io.micrometer:micrometer-registry-otlp")
-	runtimeOnly("org.postgresql:postgresql")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.springframework.security:spring-security-test")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:postgresql")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.liquibase:liquibase-core")
+    implementation("org.springframework.session:spring-session-core")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("io.micrometer:micrometer-registry-otlp")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    // https://kotlinlang.org/docs/gradle-compiler-options.html#how-to-define-options
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+        // https://kotlinlang.org/docs/java-interop.html#compiler-configuration
+        // https://docs.spring.io/spring-framework/reference/languages/kotlin/null-safety.html
+    }
 }
 
 allOpen {
-	annotation("jakarta.persistence.Entity")
-	annotation("jakarta.persistence.MappedSuperclass")
-	annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+/*
+ * Configure the resource processing tasks to expand project.properties into configuration files named
+ * `application.yaml` or `application-*.yaml`.
+ *
+ * Each file matching this naming criteria, is transformed using the `groovy.text.SimpleTemplateEngine`. This allows the
+ * use of property references like `$property` or `${property}` as well as arbitrary Groovy code in the files.
+ *
+ * See https://docs.gradle.org/current/kotlin-dsl/gradle/org.gradle.api.file/-content-filterable/expand.html
+ */
+tasks.withType<ProcessResources> {
+    outputs.upToDateWhen { false }
+    // force the process resources task to always run
+
+    filesMatching(
+        listOf(
+            "**/application.yaml",
+            "**/application-*.yaml",
+            "**/application.yml",
+            "**/application-*.yml",
+        )
+    ) {
+        logger.debug("Processing resource: {}", this.path)
+
+        expand(project.properties)
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+
+    // Always generate the jacoco coverage report after tests were run.
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.withType<JacocoReport> {
+    // Ensure that test run is up-to-date, before the jacoco coverage report is generated.
+    dependsOn(tasks.test)
+
+    classDirectories.setFrom(
+        files(
+            classDirectories.files.map {
+                fileTree(it) {
+                    exclude(
+                        "**/generated/**/*.class"
+                    )
+                }
+            }
+        )
+    )
+
+    reports {
+        xml.required.set(true)
+        // enable xml output format
+        html.required.set(true)
+        // enable html output format
+        csv.required.set(true)
+        // enable csv output format
+    }
+}
+
+/*
+ * https://docs.gradle.org/current/dsl/org.gradle.plugins.ide.idea.model.IdeaModule.html
+ */
+idea {
+    module {
+        isDownloadJavadoc = true
+        // always download docs from dependencies
+        isDownloadSources = true
+        // always download source from dependencies
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
 rootProject.name = "doppelkopf"
+
+dependencyResolutionManagement {
+    /*
+     * Centralize the repository declaration for the `buildSrc` using the (incubating) feature described in
+     * https://docs.gradle.org/current/userguide/declaring_repositories.html#sub:centralized-repository-declaration
+     */
+    @Suppress("UnstableApiUsage")
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+include("")
