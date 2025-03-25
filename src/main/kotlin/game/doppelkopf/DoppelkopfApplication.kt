872a132cb64e445c00056c3d5c0de15206c5deb8
jengryg <62914315+jengryg@users.noreply.github.com>
2025-03-25 17:12:17 +0100

    Configure Spring Boot package scans.

--- src/main/kotlin/game/doppelkopf/DoppelkopfApplication.kt
+++ src/main/kotlin/game/doppelkopf/DoppelkopfApplication.kt
@@ -1,11 +1,25 @@
 package game.doppelkopf
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+/**
+ * Configure the spring boot application to scan for components using the [DoppelkopfPackageMarker] for type safety and
+ * better IDE checking.
+ */
+@SpringBootApplication(
+    scanBasePackageClasses = [
+        DoppelkopfPackageMarker::class
+    ]
+)
+@ConfigurationPropertiesScan(
+    basePackageClasses = [
+        DoppelkopfPackageMarker::class
+    ]
+)
 class DoppelkopfApplication
 
 fun main(args: Array<String>) {
-	runApplication<DoppelkopfApplication>(*args)
+    runApplication<DoppelkopfApplication>(*args)
 }
---
+++ src/main/kotlin/game/doppelkopf/DoppelkopfPackageMarker.kt
@@ -0,0 +1,7 @@
+package game.doppelkopf
+
+/**
+ * Marker interface to reference the [game.doppelkopf] package in a type safe way, when using spring annotations with
+ * package scanning like `@ConfigurationPropertiesScan` or `@SpringBootApplication`.
+ */
+interface DoppelkopfPackageMarker
