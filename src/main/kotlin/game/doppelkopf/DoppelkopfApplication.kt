a6b1e38ddad0b5261d17d776bb8806891d0124bc
jengryg <62914315+jengryg@users.noreply.github.com>
2025-03-25 16:16:18 +0100

    Add spring initializer files.

---
+++ src/main/kotlin/game/doppelkopf/DoppelkopfApplication.kt
@@ -0,0 +1,11 @@
+package game.doppelkopf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DoppelkopfApplication
+
+fun main(args: Array<String>) {
+	runApplication<DoppelkopfApplication>(*args)
+}
---
+++ src/test/kotlin/game/doppelkopf/TestDoppelkopfApplication.kt
@@ -0,0 +1,9 @@
+package game.doppelkopf
+
+import org.springframework.boot.fromApplication
+import org.springframework.boot.with
+
+
+fun main(args: Array<String>) {
+	fromApplication<DoppelkopfApplication>().with(TestcontainersConfiguration::class).run(*args)
+}
