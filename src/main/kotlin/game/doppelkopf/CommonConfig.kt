1cafd2c7753d88f49a268329e14f8cc148d75474
jengryg <62914315+jengryg@users.noreply.github.com>
2025-03-26 01:06:46 +0100

    Add common configuration for stage and version information.

---
+++ src/main/kotlin/game/doppelkopf/CommonConfig.kt
@@ -0,0 +1,35 @@
+package game.doppelkopf
+
+import game.doppelkopf.instrumentation.logging.Logging
+import game.doppelkopf.instrumentation.logging.logger
+import jakarta.validation.constraints.Pattern
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.bind.ConstructorBinding
+import org.springframework.validation.annotation.Validated
+
+@Validated
+@ConfigurationProperties(prefix = "app.common")
+class CommonConfig @ConstructorBinding constructor(
+    /**
+     * The stage identifier.
+     */
+    @field:Pattern(regexp = "^[0-9A-Za-z]+\$")
+    val stage: String,
+
+    /**
+     * The version of the project.
+     */
+    @field:Pattern(regexp = "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?\$")
+    val version: String? = null,
+) : Logging {
+    private val log = logger()
+
+    init {
+        log.atInfo()
+            .setMessage("CommonConfig initialized.")
+            .addKeyValue("stage") { stage }
+            .addKeyValue("version") { version }
+            .log()
+
+    }
+}
