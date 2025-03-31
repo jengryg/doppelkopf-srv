cc4fa9869f042489d0a7f3fb401c1c8641710f10
jengryg <62914315+jengryg@users.noreply.github.com>
2025-04-01 01:45:50 +0200

    Add Result extension function for ForbiddenActionException and InvalidActionException.

--- src/main/kotlin/game/doppelkopf/core/errors/ForbiddenActionException.kt
+++ src/main/kotlin/game/doppelkopf/core/errors/ForbiddenActionException.kt
@@ -20,3 +20,21 @@ class ForbiddenActionException(
         }
     }
 }
+
+/**
+ * Instantiate [ForbiddenActionException] inside a [Result.failure].
+ *
+ * @param T the type of the wrapped value in [Result]
+ * @param action see [ForbiddenActionException]
+ * @param reason see [ForbiddenActionException]
+ * @param cause see [ForbiddenActionException]
+ *
+ * @return a [Result] wrapping type [T] in [Result.failure] state with an [ForbiddenActionException]
+ */
+fun <T> Result.Companion.ofForbiddenAction(
+    action: String,
+    reason: String? = null,
+    cause: Throwable? = null
+): Result<T> {
+    return failure(ForbiddenActionException(action, reason, cause))
+}
--- src/main/kotlin/game/doppelkopf/core/errors/InvalidActionException.kt
+++ src/main/kotlin/game/doppelkopf/core/errors/InvalidActionException.kt
@@ -20,3 +20,21 @@ class InvalidActionException(
         }
     }
 }
+
+/**
+ * Instantiate [InvalidActionException] inside a [Result.failure].
+ *
+ * @param T the type of the wrapped value in [Result]
+ * @param action see [InvalidActionException]
+ * @param reason see [InvalidActionException]
+ * @param cause see [InvalidActionException]
+ *
+ * @return a [Result] wrapping type [T] in [Result.failure] state with an [InvalidActionException]
+ */
+fun <T> Result.Companion.ofInvalidAction(
+    action: String,
+    reason: String? = null,
+    cause: Throwable? = null
+): Result<T> {
+    return failure(InvalidActionException(action, reason, cause))
+}
