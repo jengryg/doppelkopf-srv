4b623a48bd05a0c4fd6ba1a53c5a6d5c9f5dc19d
jengryg <62914315+jengryg@users.noreply.github.com>
2025-03-31 19:01:52 +0200

    Improve GameControllerTest and PlayerControllerTest. Decoupled testing and success/error case coverage.

--- src/test/kotlin/game/doppelkopf/api/game/GameControllerTest.kt
+++ src/test/kotlin/game/doppelkopf/api/game/GameControllerTest.kt
@@ -12,9 +12,11 @@ import game.doppelkopf.errors.ProblemDetailResponse
 import game.doppelkopf.persistence.game.GameEntity
 import game.doppelkopf.persistence.game.GameRepository
 import game.doppelkopf.persistence.game.PlayerEntity
+import game.doppelkopf.persistence.user.UserEntity
 import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -37,157 +39,192 @@ class GameControllerTest : BaseRestAssuredTest() {
         }
     }
 
-    @Test
-    fun `get list with no games in database returns 200 empty list`() {
-        gameRepository.deleteAll()
+    @Nested
+    inner class GetAndList {
+        @Test
+        fun `get list with no games in database returns 200 empty list`() {
+            gameRepository.deleteAll()
 
-        val response = getResourceList<GameInfoDto>("/v1/games", 200)
+            val response = getResourceList<GameInfoDto>("/v1/games", 200)
 
-        assertThat(response).isEmpty()
-    }
-
-    @Test
-    fun `get list with some games in the database returns 200 with list of dto`() {
-        gameRepository.deleteAll()
-        val entities = listOf(
-            GameEntity(creator = testUser, maxNumberOfPlayers = 4),
-            GameEntity(creator = testUser, maxNumberOfPlayers = 5),
-            GameEntity(creator = testAdmin, maxNumberOfPlayers = 6),
-            GameEntity(creator = testAdmin, maxNumberOfPlayers = 7),
-        ).map {
-            gameRepository.save(it)
+            assertThat(response).isEmpty()
         }
 
-        val response = getResourceList<GameInfoDto>("/v1/games", 200)
-        assertThat(response).hasSize(4)
+        @Test
+        fun `get list with some games in the database returns 200 with list of dto`() {
+            gameRepository.deleteAll()
+            val entities = listOf(
+                createGameOfUser(testUser),
+                createGameOfUser(testUser),
+                createGameOfUser(testAdmin),
+                createGameOfUser(testAdmin)
+            ).map {
+                gameRepository.save(it)
+            }
 
-        assertThat(response.map { it.id }).containsExactlyInAnyOrderElementsOf(entities.map { it.id })
-    }
+            val response = getResourceList<GameInfoDto>("/v1/games", 200)
+            assertThat(response).hasSize(4)
 
-    @Test
-    fun `get specific with unknown uuid returns 404`() {
-        val response = getResource<ProblemDetailResponse>(
-            path = "/v1/games/$zeroId",
-            expectedStatus = 404
-        )
-
-        assertThat(response.instance.toString()).isEqualTo("/v1/games/$zeroId")
-        assertThat(response.title).isEqualTo("Entity not found")
-        assertThat(response.detail).isEqualTo("The entity of type GameEntity with id $zeroId was not found.")
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = [4, 5, 6, 7, 8])
-    fun `create with valid player limit returns 201 and dto`(playerLimit: Int) {
-        val (response, location) = execCreateGame<GameInfoDto>(playerLimit, 201)
-
-        response.also {
-            assertThat(it.creator.id).isEqualTo(testUser.id)
-            assertThat(it.playerLimit).isEqualTo(playerLimit)
-
-            // Ensure that the creator was automatically added as player in seat position 0.
-            assertThat(it.players).hasSize(1)
-            assertThat(it.players.map { p -> p.user.id }).containsExactly(testUser.id)
-            assertThat(it.players.map { p -> p.seat }).containsExactly(0)
+            assertThat(response.map { it.id }).containsExactlyInAnyOrderElementsOf(entities.map { it.id })
         }
 
+        @Test
+        fun `get specific with unknown uuid returns 404`() {
+            val response = getResource<ProblemDetailResponse>(
+                path = "/v1/games/$zeroId",
+                expectedStatus = 404
+            )
 
-        assertThat(location).isNotNull()
+            assertThat(response.instance.toString()).isEqualTo("/v1/games/$zeroId")
+            assertThat(response.title).isEqualTo("Entity not found")
+            assertThat(response.detail).isEqualTo("The entity of type GameEntity with id $zeroId was not found.")
+        }
 
-        getResource<GameInfoDto>(location!!, 200).also {
-            assertThat(it.creator.id).isEqualTo(testUser.id)
-            assertThat(it.playerLimit).isEqualTo(playerLimit)
+        @Test
+        fun `get specific by its id returns 200 and dto`() {
+            val entity = createGameOfUser(testUser).apply {
+                players.add(PlayerEntity(user = testPlayers[0], game = this, seat = 1))
+                players.add(PlayerEntity(user = testPlayers[1], game = this, seat = 2))
+                players.add(PlayerEntity(user = testPlayers[2], game = this, seat = 3))
+            }.let {
+                gameRepository.save(it)
+            }
 
-            // Ensure that the creator was automatically added as player in seat position 0.
-            assertThat(it.players).hasSize(1)
-            assertThat(it.players.map { p -> p.user.id }).containsExactly(testUser.id)
-            assertThat(it.players.map { p -> p.seat }).containsExactly(0)
+            val response = getResource<GameInfoDto>(
+                path = "/v1/games/${entity.id}",
+                expectedStatus = 200
+            )
+
+            assertThat(response.id).isEqualTo(entity.id)
+            assertThat(response.creator.id).isEqualTo(entity.creator.id)
+            assertThat(response.playerLimit).isEqualTo(entity.maxNumberOfPlayers)
+            assertThat(response.players).hasSize(4)
+            assertThat(response.players.map { Pair(it.user.id, it.seat) }).containsExactlyInAnyOrder(
+                Pair(testUser.id, 0), Pair(testPlayers[0].id, 1), Pair(testPlayers[1].id, 2), Pair(testPlayers[2].id, 3)
+            )
         }
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = [0, 1, 2, 3, -17, 42])
-    fun `create with invalid player limit returns 400 bad request`(playerLimit: Int) {
-        val (response, location) = execCreateGame<ProblemDetailResponse>(playerLimit, 400)
+    @Nested
+    inner class Creation {
+        @ParameterizedTest
+        @ValueSource(ints = [4, 5, 6, 7, 8])
+        fun `create with valid player limit returns 201 and dto`(playerLimit: Int) {
+            val (response, location) = execCreateGame<GameInfoDto>(playerLimit, 201)
 
-        assertThat(response.instance.toString()).isEqualTo("/v1/games")
-        assertThat(response.title).isEqualTo("Bad Request")
-        assertThat(response.detail).isEqualTo("Invalid request content.")
+            response.also {
+                assertThat(it.creator.id).isEqualTo(testUser.id)
+                assertThat(it.playerLimit).isEqualTo(playerLimit)
 
-        assertThat(location).isNull()
+                // Ensure that the creator was automatically added as player in seat position 0.
+                assertThat(it.players).hasSize(1)
+                assertThat(it.players.map { p -> p.user.id }).containsExactly(testUser.id)
+                assertThat(it.players.map { p -> p.seat }).containsExactly(0)
+            }
+
+
+            assertThat(location).isNotNull()
+
+            getResource<GameInfoDto>(location!!, 200).also {
+                assertThat(it.creator.id).isEqualTo(testUser.id)
+                assertThat(it.playerLimit).isEqualTo(playerLimit)
+
+                // Ensure that the creator was automatically added as player in seat position 0.
+                assertThat(it.players).hasSize(1)
+                assertThat(it.players.map { p -> p.user.id }).containsExactly(testUser.id)
+                assertThat(it.players.map { p -> p.seat }).containsExactly(0)
+            }
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = [0, 1, 2, 3, -17, 42])
+        fun `create with invalid player limit returns 400 bad request`(playerLimit: Int) {
+            val (response, location) = execCreateGame<ProblemDetailResponse>(playerLimit, 400)
+
+            assertThat(response.instance.toString()).isEqualTo("/v1/games")
+            assertThat(response.title).isEqualTo("Bad Request")
+            assertThat(response.detail).isEqualTo("Invalid request content.")
+
+            assertThat(location).isNull()
+        }
     }
 
-    @Test
-    fun `get specific by its id returns 200 and dto`() {
-        val entity = GameEntity(
-            creator = testAdmin,
-            maxNumberOfPlayers = 8
-        ).apply {
-            testPlayers.forEachIndexed { index, player ->
-                players.add(
-                    PlayerEntity(
-                        user = player,
-                        game = this,
-                        seat = index
-                    )
+
+    @Nested
+    inner class Starting {
+        @Test
+        fun `start returns 200 when model returns successful`() {
+            every { gameModelFactory.create(any()) } returns mockk {
+                every { start(testUser) } returns Unit
+                // the starting method of the model is tested in the model test, we just mock it here
+            }
+
+            val entity = createGameOfUser(testUser).let {
+                gameRepository.save(it)
+            }
+
+            val response = execPatchGame<GameInfoDto>(entity.id, GameOperation.START, 200)
+
+            assertThat(response.id).isEqualTo(entity.id)
+            assertThat(response.creator.id).isEqualTo(entity.creator.id)
+            assertThat(response.playerLimit).isEqualTo(entity.maxNumberOfPlayers)
+            assertThat(response.players).hasSize(entity.players.size)
+        }
+
+        @Test
+        fun `start returns 400 when invalid action exception`() {
+            every { gameModelFactory.create(any()) } returns mockk {
+                every { start(testUser) } throws InvalidActionException(
+                    "Game:Start",
+                    "Mocked Model Exception."
                 )
             }
-        }.let { gameRepository.save(it) }
 
-        val response = getResource<GameInfoDto>(
-            path = "/v1/games/${entity.id}",
-            expectedStatus = 200
-        )
+            val entity = createGameOfUser(testUser).let {
+                gameRepository.save(it)
+            }
 
-        assertThat(response.id).isEqualTo(entity.id)
-        assertThat(response.creator.id).isEqualTo(entity.creator.id)
-        assertThat(response.playerLimit).isEqualTo(entity.players.size)
-        assertThat(
-            response.players.map { it.user.id }
-        ).containsExactlyInAnyOrderElementsOf(testPlayers.map { it.id })
+            val response = execPatchGame<ProblemDetailResponse>(entity.id, GameOperation.START, 400)
+
+            response.also {
+                assertThat(response.instance.toString()).isEqualTo("/v1/games/${entity.id}")
+                assertThat(response.title).isEqualTo("Invalid action")
+                assertThat(response.detail).isEqualTo("The action 'Game:Start' can not be performed: Mocked Model Exception.")
+            }
+        }
+
+        @Test
+        fun `start returns 403 when forbidden action exception`() {
+            every { gameModelFactory.create(any()) } returns mockk {
+                every { start(testUser) } throws ForbiddenActionException(
+                    "Game:Start",
+                    "Mocked Model Exception."
+                )
+            }
+
+            val entity = createGameOfUser(testAdmin).let {
+                gameRepository.save(it)
+            }
+
+            val response = execPatchGame<ProblemDetailResponse>(entity.id, GameOperation.START, 403)
+
+            response.also {
+                assertThat(response.instance.toString()).isEqualTo("/v1/games/${entity.id}")
+                assertThat(response.title).isEqualTo("Forbidden action")
+                assertThat(response.detail).isEqualTo("You are not allowed to perform the action 'Game:Start': Mocked Model Exception.")
+            }
+        }
     }
 
-    @Test
-    fun `start returns 400 when invalid action exception`() {
-        every { gameModelFactory.create(any()) } returns mockk {
-            every { start(testUser) } throws InvalidActionException(
-                "Game:Start",
-                "Mocked Model Exception."
+    private fun createGameOfUser(userEntity: UserEntity): GameEntity {
+        return GameEntity(
+            creator = userEntity, maxNumberOfPlayers = 4
+        ).apply {
+            players.add(
+                PlayerEntity(
+                    user = userEntity, game = this, seat = 0
+                )
             )
-        }
-
-        val game = GameEntity(creator = testUser, maxNumberOfPlayers = 4).let {
-            gameRepository.save(it)
-        }
-
-        val response = execPatchGame<ProblemDetailResponse>(game.id, GameOperation.START, 400)
-
-        response.also {
-            assertThat(response.instance.toString()).isEqualTo("/v1/games/${game.id}")
-            assertThat(response.title).isEqualTo("Invalid action")
-            assertThat(response.detail).isEqualTo("The action 'Game:Start' can not be performed: Mocked Model Exception.")
-        }
-    }
-
-    @Test
-    fun `start returns 403 when forbidden action exception`() {
-        every { gameModelFactory.create(any()) } returns mockk {
-            every { start(testUser) } throws ForbiddenActionException(
-                "Game:Start",
-                "Mocked Model Exception."
-            )
-        }
-
-        val game = GameEntity(creator = testUser, maxNumberOfPlayers = 4).let {
-            gameRepository.save(it)
-        }
-
-        val response = execPatchGame<ProblemDetailResponse>(game.id, GameOperation.START, 403)
-
-        response.also {
-            assertThat(response.instance.toString()).isEqualTo("/v1/games/${game.id}")
-            assertThat(response.title).isEqualTo("Forbidden action")
-            assertThat(response.detail).isEqualTo("You are not allowed to perform the action 'Game:Start': Mocked Model Exception.")
         }
     }
 
--- src/test/kotlin/game/doppelkopf/api/game/PlayerControllerTest.kt
+++ src/test/kotlin/game/doppelkopf/api/game/PlayerControllerTest.kt
@@ -14,6 +14,7 @@ import game.doppelkopf.persistence.user.UserEntity
 import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -36,112 +37,137 @@ class PlayerControllerTest : BaseRestAssuredTest() {
         }
     }
 
-    @Test
-    fun `get players of game returns 200 and list of dto`() {
-        val game = GameEntity(
-            creator = testPlayers[0], maxNumberOfPlayers = 8
-        ).also { game ->
-            testPlayers.mapIndexed { index, userEntity ->
-                game.players.add(PlayerEntity(user = userEntity, game = game, seat = index))
+    @Nested
+    inner class GetAndList {
+        @Test
+        fun `get players of game returns 200 and list of dto`() {
+            val game = GameEntity(
+                creator = testPlayers[0], maxNumberOfPlayers = 8
+            ).also { game ->
+                testPlayers.mapIndexed { index, userEntity ->
+                    game.players.add(PlayerEntity(user = userEntity, game = game, seat = index))
+                }
+            }.let {
+                gameRepository.save(it)
             }
-        }.let {
-            gameRepository.save(it)
+
+            val response = getResourceList<PlayerInfoDto>("/v1/games/${game.id}/players", 200)
+
+            assertThat(response).hasSize(8)
+
+            assertThat(response.map { it.id }).containsExactlyInAnyOrderElementsOf(game.players.map { it.id })
         }
 
-        val response = getResourceList<PlayerInfoDto>("/v1/games/${game.id}/players", 200)
+        @Test
+        fun `get players of game with unknown id returns 404`() {
+            val response = getResource<ProblemDetailResponse>("/v1/games/$zeroId/players", 404)
 
-        assertThat(response).hasSize(8)
+            assertThat(response.instance.toString()).isEqualTo("/v1/games/$zeroId/players")
+            assertThat(response.title).isEqualTo("Entity not found")
+            assertThat(response.detail).isEqualTo("The entity of type GameEntity with id $zeroId was not found.")
+        }
 
-        assertThat(response.map { it.id }).containsExactlyInAnyOrderElementsOf(game.players.map { it.id })
+        @Test
+        fun `get speicifc by its id returns 200 and dto`() {
+            val entity = createGameOfUser(testUser).let {
+                gameRepository.save(it)
+            }
+
+            val player = entity.players.first()
+
+            val response = getResource<PlayerInfoDto>("/v1/players/${player.id}", 200)
+
+            response.also {
+                assertThat(it.id).isEqualTo(player.id)
+                assertThat(it.gameId).isEqualTo(entity.id)
+                assertThat(it.seat).isEqualTo(player.seat)
+                assertThat(it.dealer).isFalse()
+                assertThat(it.user.id).isEqualTo(player.user.id)
+            }
+        }
     }
 
-    @Test
-    fun `get players of game with unknown id returns 404`() {
-        val response = getResource<ProblemDetailResponse>("/v1/games/$zeroId/players", 404)
+    @Nested
+    inner class Join {
+        @Test
+        fun `join returns 400 when invalid action exception`() {
+            every { gameModelFactory.create(any()) } returns mockk {
+                every { join(testUser, 3) } throws InvalidActionException(
+                    "Game:Join",
+                    "Mocked Model Exception."
+                )
+            }
 
-        assertThat(response.instance.toString()).isEqualTo("/v1/games/$zeroId/players")
-        assertThat(response.title).isEqualTo("Entity not found")
-        assertThat(response.detail).isEqualTo("The entity of type GameEntity with id $zeroId was not found.")
-    }
+            val game = createGameOfUser(testAdmin).let {
+                gameRepository.save(it)
+            }
 
-    @Test
-    fun `join returns 400 when invalid action exception`() {
-        every { gameModelFactory.create(any()) } returns mockk {
-            every { join(testUser, 3) } throws InvalidActionException(
-                "Game:Join",
-                "Mocked Model Exception."
-            )
+            val (response, location) = execJoinGame<ProblemDetailResponse>(game.id, 3, 400)
+
+            assertThat(response.instance.toString()).isEqualTo("/v1/games/${game.id}/players")
+            assertThat(response.title).isEqualTo("Invalid action")
+            assertThat(response.detail).isEqualTo("The action 'Game:Join' can not be performed: Mocked Model Exception.")
+
+            assertThat(location).isNull()
         }
 
-        val game = createGameOfUser(testAdmin).let {
-            gameRepository.save(it)
+        @Test
+        fun `join returns 403 when forbidden action exception`() {
+            every { gameModelFactory.create(any()) } returns mockk {
+                every { join(testUser, 3) } throws ForbiddenActionException(
+                    "Game:Join",
+                    "Mocked Model Exception."
+                )
+            }
+
+            val game = createGameOfUser(testAdmin).let {
+                gameRepository.save(it)
+            }
+
+            val (response, location) = execJoinGame<ProblemDetailResponse>(game.id, 3, 403)
+
+            assertThat(response.instance.toString()).isEqualTo("/v1/games/${game.id}/players")
+            assertThat(response.title).isEqualTo("Forbidden action")
+            assertThat(response.detail).isEqualTo("You are not allowed to perform the action 'Game:Join': Mocked Model Exception.")
+
+            assertThat(location).isNull()
         }
 
-        val (response, location) = execJoinGame<ProblemDetailResponse>(game.id, 3, 400)
+        @ParameterizedTest
+        @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7])
+        fun `join game at valid seat position returns 201 and dto`(seat: Int) {
+            val game = gameRepository.save(createGameOfUser(testAdmin))
+            val player = PlayerEntity(user = testUser, game = game, seat = seat)
 
-        assertThat(response.instance.toString()).isEqualTo("/v1/games/${game.id}/players")
-        assertThat(response.title).isEqualTo("Invalid action")
-        assertThat(response.detail).isEqualTo("The action 'Game:Join' can not be performed: Mocked Model Exception.")
+            every { gameModelFactory.create(any()) } returns mockk {
+                every { join(testUser, seat) } returns player
+            }
 
-        assertThat(location).isNull()
-    }
+            val (response, location) = execJoinGame<PlayerInfoDto>(game.id, seat, 201)
 
-    @Test
-    fun `join returns 403 when forbidden action exception`() {
-        every { gameModelFactory.create(any()) } returns mockk {
-            every { join(testUser, 3) } throws ForbiddenActionException(
-                "Game:Join",
-                "Mocked Model Exception."
-            )
+            response.also {
+                assertThat(it.id).isEqualTo(player.id)
+                assertThat(it.user.id).isEqualTo(player.user.id)
+                assertThat(it.seat).isEqualTo(player.seat)
+                assertThat(it.gameId).isEqualTo(player.game.id)
+            }
+
+            assertThat(location).isEqualTo("/v1/players/${player.id}")
         }
 
-        val game = createGameOfUser(testAdmin).let {
-            gameRepository.save(it)
+        @ParameterizedTest
+        @ValueSource(ints = [0, 8, -17, 42])
+        fun `join game at invalid seat position returns 400 bad request`(seat: Int) {
+            val game = gameRepository.save(createGameOfUser(testAdmin))
+
+            val (response, location) = execJoinGame<ProblemDetailResponse>(game.id, seat, 400)
+
+            assertThat(response.instance.toString()).isEqualTo("/v1/games/${game.id}/players")
+            assertThat(response.title).isEqualTo("Bad Request")
+            assertThat(response.detail).isEqualTo("Invalid request content.")
+
+            assertThat(location).isNull()
         }
-
-        val (response, location) = execJoinGame<ProblemDetailResponse>(game.id, 3, 403)
-
-        assertThat(response.instance.toString()).isEqualTo("/v1/games/${game.id}/players")
-        assertThat(response.title).isEqualTo("Forbidden action")
-        assertThat(response.detail).isEqualTo("You are not allowed to perform the action 'Game:Join': Mocked Model Exception.")
-
-        assertThat(location).isNull()
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7])
-    fun `join game at valid seat position returns 201 and dto`(seat: Int) {
-        val game = gameRepository.save(createGameOfUser(testAdmin))
-        val player = PlayerEntity(user = testUser, game = game, seat = seat)
-
-        every { gameModelFactory.create(any()) } returns mockk {
-            every { join(testUser, seat) } returns player
-        }
-
-        val (response, location) = execJoinGame<PlayerInfoDto>(game.id, seat, 201)
-
-        response.also {
-            assertThat(it.id).isEqualTo(player.id)
-            assertThat(it.user.id).isEqualTo(player.user.id)
-            assertThat(it.seat).isEqualTo(player.seat)
-            assertThat(it.gameId).isEqualTo(player.game.id)
-        }
-
-        assertThat(location).isEqualTo("/v1/players/${player.id}")
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = [0, 8, -17, 42])
-    fun `join game at invalid seat position returns 400 bad request`(seat: Int) {
-        val game = gameRepository.save(createGameOfUser(testAdmin))
-
-        val (response, location) = execJoinGame<ProblemDetailResponse>(game.id, seat, 400)
-
-        assertThat(response.instance.toString()).isEqualTo("/v1/games/${game.id}/players")
-        assertThat(response.title).isEqualTo("Bad Request")
-        assertThat(response.detail).isEqualTo("Invalid request content.")
-
-        assertThat(location).isNull()
     }
 
     private fun createGameOfUser(userEntity: UserEntity): GameEntity {
