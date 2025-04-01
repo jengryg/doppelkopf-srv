0972ec0685ae01e35579000db7674752892f6be7
jengryg <62914315+jengryg@users.noreply.github.com>
2025-04-01 20:28:27 +0200

    Introduce PublicDeclaration to hide silent marriage from other players.

--- src/main/kotlin/game/doppelkopf/core/play/enums/Declaration.kt
+++ src/main/kotlin/game/doppelkopf/core/play/enums/Declaration.kt
@@ -1,8 +1,23 @@
 package game.doppelkopf.core.play.enums
 
-enum class Declaration {
+/**
+ * [Declaration] defines the options players have to declare their hand.
+ * The [publicDeclaration] represents the corresponding public representation of the choice, to hide [SILENT_MARRIAGE]
+ * options from the other players.
+ */
+enum class Declaration(val publicDeclaration: PublicDeclaration) {
+    NOTHING(PublicDeclaration.NOTHING),
+    HEALTHY(PublicDeclaration.HEALTHY),
+    SILENT_MARRIAGE(PublicDeclaration.HEALTHY),
+    RESERVATION(PublicDeclaration.RESERVATION),
+}
+
+/**
+ * [PublicDeclaration] is used to communicate the choice of the players to each other.
+ * It hides the [Declaration.SILENT_MARRIAGE] option, that must not be shown to other players.
+ */
+enum class PublicDeclaration {
     NOTHING,
     HEALTHY,
-    SILENT_MARRIAGE,
     RESERVATION,
 }
